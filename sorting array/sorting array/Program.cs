71a948a277f1a5c20c47060527a6493b63efb76f
tejaswini gj <tejaswinigpj@gmail.com>
2020-09-13 12:52:25 +0530

    sorting array program

---
+++ sorting array/sorting array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 98, 23, 97, 36, 77, 56, 89, 10, 87, 96 };
+            Console.WriteLine("Unsorted array");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Array.Sort(list);
+            Console.WriteLine("\nSorted List");
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sorting array/sorting array/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sorting array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("sorting array")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dbaefff8-f07f-4356-9793-8273d68f558e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
