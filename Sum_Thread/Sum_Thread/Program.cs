299abffdb96017764479f7be0f1fa5d8e693b458
tejaswini gj <tejaswinigpj@gmail.com>
2020-12-11 15:59:35 +0530

    threading concepts

---
+++ Sum_Thread/Sum_Thread/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Sum_Thread
+{
+    class MyThread
+    {
+        private int sum, sum1;
+
+        public int Sum { get => sum; }
+        public int Sum1 { get => sum1; }
+
+        public void Sum_Thread1Method() //(object data) :parameter
+        {
+          //int startValue = (int)data
+            int n;
+            for(n=1;n<=50;n++)
+            {
+                sum = sum + n;
+            }
+            Console.WriteLine("Sum=" + sum);
+        }
+
+        public void Sum_Thread2Method()
+        {
+            int n;
+            for (n = 51; n <= 100; n++)
+            {
+                sum1 = sum1 + n;
+            }
+            Console.WriteLine("Sum=" + sum1);
+        }
+
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyThread demoThread = new MyThread();
+            Thread  Sum_Thread1 = new Thread(new ThreadStart(demoThread.Sum_Thread1Method));
+            //pass the parameter
+         // Thread Sum_Thread1 = new Thread((demoThread.Sum_Thread1Method));
+            Thread Sum_Thread2 = new Thread(new ThreadStart(demoThread.Sum_Thread2Method));
+            Sum_Thread1.Start(); //pass the value  (10)
+            Sum_Thread2.Start();
+
+            Sum_Thread1.Join();
+            Sum_Thread2.Join();
+            int summation= demoThread.Sum + demoThread.Sum1;
+
+
+            Console.WriteLine("Total:" + summation);
+            Console.ReadLine();
+        }
+    }
+}
--- Thread1/Thread1/Program.cs
+++ Thread1/Thread1/Program.cs
@@ -10,8 +10,9 @@ namespace Thread1
     class MyThread
     {
         //this method  will be executed when you start the thread1
-        public void Thread1Method()
+        public void Thread1Method(object state)
         {
+            Console.Write(Thread.CurrentThread.Name);
             int t1=100;
             for(int i=1;i<t1;i+=2)
             {
@@ -20,8 +21,9 @@ namespace Thread1
         }
 
         //this method  will be executed when you start the thread2
-        public void Thread2Method()
+        public void Thread2Method(object state)
         {
+            Console.Write(Thread.CurrentThread.Name);
             int t2 = 100;
             for (int i = 2; i < t2; i += 2)
             {
@@ -34,11 +36,26 @@ namespace Thread1
         static void Main(string[] args)
         {
             MyThread demoThread = new MyThread();
-            Thread thread1 = new Thread(new ThreadStart(demoThread.Thread1Method));
-            Thread thread2 = new Thread(new ThreadStart(demoThread.Thread2Method));
-            //thread1.Start();
-            thread2.Start();
 
+            //Thread Creation
+           /* Thread thread1 = new Thread(new ThreadStart(demoThread.Thread1Method));
+             Thread1.Name = "Thread1";
+            Thread thread2 = new Thread(new ThreadStart(demoThread.Thread2Method));
+            Thread2.Name = "Thread2";
+            
+            //start the threads
+            thread1.Start();
+            thread2.Start();
+            Console.WriteLine("Name of the Thread1"+thread1.Name);
+            Console.WriteLine("Name of the Thread1"+thread1.Name);
+            
+            //after starting both the thread the main thread will continue
+            thread1.Join();
+            thread2.Join();*/
+
+           // ThreadPool.QueueUserWorkItem(demoThread.Thread1Method);
+           // ThreadPool.QueueUserWorkItem(demoThread.Thread2Method);
+            Console.WriteLine("main thread is exiting");
             Console.ReadLine();
         }
     }
